28b5f56651ce1f720d9ab307f8059cad6dac74d6
veronika667700 <veronika-saakova@rambler.ru>
2015-12-25 14:45:02 +0300

    commit

---
+++ app/src/main/java/com/masstersoft/myapplication/MainActivity.java
@@ -0,0 +1,72 @@
+package com.masstersoft.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolBar;
+    private DrawerLayout drawerlayout;
+    private Button btnStart;
+    private Button btnStop;//,k,ik,k,ok,
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnStart=(Button)findViewById(R.id.btn1);
+
+        initToolBar();
+        initNavigationView();
+    }
+
+    private void initNavigationView() {
+        drawerlayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+    }
+
+    private void initToolBar() {
+        toolBar = (Toolbar) findViewById(R.id.toolbar);
+        toolBar.setTitle(R.string.app_name);
+        toolBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolBar.inflateMenu(R.menu.menu_main);
+        setSupportActionBar(toolBar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickBtn(View view) {
+        btnStart.setText("Fucking button");
+    }
+}
